8a2ef88f67609f357c0f0463e51ebb2418dcd663
Rifqi Maulana <abelcaster025@gmail.com>
2023-03-23 10:04:15 +0700

    [Structure/UI] Delete .env, Change UI Font, Initiate some comments

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '@/styles/globals.css'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat(
+  {
+    subsets: ['latin'],
+    variable: '--font-montserrat'
+  }
+)
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <main className={`${montserrat.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
 }
